64dea77b39d980109eaaf57a864fcac0e1960719
JK2548 <JK2548@JK2548-NB>
2016-10-05 19:02:05 +0700

    - update 05102016 19:01     FIX_1 update main page show risk for user

--- pages/user/main.php
+++ pages/user/main.php
@@ -23,18 +23,12 @@
                             </thead>
                             <tbody>
                                 <?php
-                            $sqls = " SELECT 	r.*,c.cat_name,ct.sub_name as csub_name,d.dep_name,ds.sub_name as dsub_name,
-                            l.lv_name,l.lv_code,u.user_fname,t.sta_name
-                                        from tb_risk r
-                                        LEFT OUTER JOIN sys_sub_category ct ON ct.sub_cat_id = r.sub_cat_id
-                                        LEFT OUTER JOIN sys_category c ON c.cat_id = ct.cat_id
-                                        LEFT OUTER JOIN sys_sub_department ds ON ds.sub_dep_id = r.sub_dep_id
-                                        LEFT OUTER JOIN sys_department d ON d.dep_id = ds.dep_id
-                                        LEFT OUTER JOIN sys_level_risk l ON l.lv_id = r.lv_id
-                                        LEFT OUTER JOIN sys_status_risk t ON t.sta_id = r.risk_status
-                                        LEFT OUTER JOIN tb_user u ON u.user_id = r.user_id "
-                                        . " WHERE r.risk_status < '4' "
-                                        . " order by r.risk_status asc ";
+                                $sqls = " SELECT r.*,s.sta_name "
+                                        . " FROM tb_risk r "
+                                        . " left outer join sys_status_risk s ON s.sta_id = r.risk_status "
+                                        . " where user_id = '$userid' "
+                                        . " and r.risk_status < '3' "
+                                        . " order by r.risk_datetime,r.lv_code ";
                                 $results = mysql_query($sqls)or die($sqls);
                                 while ($record = mysql_fetch_array($results)) {
                     echo "<tr class= 'odd gradeX'>
